4d1be9844195e8080196620b41b344892f34bcc3
Juan Rodriguez <Juan337492@yahoo.com>
2023-09-17 15:52:47 -0400

    Posting loading status

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,8 +10,9 @@ dayjs.extend(relativeTime);
 import { api } from "~/utils/api";
 import type { RouterOutputs } from "~/utils/api";
 import Image from "next/image";
-import { LoadingPage} from "~/components/loading";
+import { LoadingPage, LoadingSpinner} from "~/components/loading";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 const CreatePostWizard = () => {
   const {user} = useUser();
@@ -25,6 +26,14 @@ const CreatePostWizard = () => {
       setInput("");
     void  ctx.posts.getAll.invalidate();
     },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if(errorMessage && errorMessage[0]){
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Faild to post! Please try again later.");
+      }
+    }       
   });
  
   console.log(user);
@@ -42,9 +51,26 @@ const CreatePostWizard = () => {
     type="text"
     value={input}
     onChange={(e) => setInput(e.target.value)}
+
+    onKeyDown={(e) => {
+      if (e.key ===  "Enter") {
+        e.preventDefault();
+        if(input !== ""){
+        mutate({content: input});
+       }
+      }
+    }
+    }
     disabled={isPosting}
     />
-    <button onClick={() => mutate({content:input})}>Post</button>
+    
+    {input !=="" && !isPosting && (
+      <button onClick={() => mutate({content:input})}>Post</button>)};
+    
+    {isPosting && 
+    <div className="flex items-center justify-center">
+    <LoadingSpinner size={20}/>
+    </div>}
   </div>);
   
 };
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -60,12 +60,12 @@ export const postsRouter = createTRPCRouter({
   create: privateProcedure
   .input(
     z.object({
-      content: z.string().emoji().min(1).max(280),
+      content: z.string().emoji("Only emojis are allowed").min(1).max(280),
     })
   )
   .mutation(async({ctx , input}) => {
     const authorId = ctx.userId;
-    
+
     const { success } = await ratelimit.limit(authorId);
     if(!success) throw new TRPCError({ code:"TOO_MANY_REQUESTS"});
 
